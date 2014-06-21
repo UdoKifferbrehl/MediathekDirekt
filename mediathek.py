c241b3e00a196e940ec7e3881b047fa5e467fdb6
Markus Koschany <apo@gambaru.de>
2014-06-21 15:34:11 +0200

    Use PEP 8 style 4 spaces

--- mediathek.py
+++ mediathek.py
@@ -41,22 +41,22 @@ print("Mediatheken - Suche: Starting download")
 xmldoc = minidom.parse('mediathek.xml')
 itemlist = xmldoc.getElementsByTagName('film-update-server-url')
 for item in itemlist[:10]:
-        try:
-                url = item.firstChild.nodeValue
-                response = urlopen(url)
-                html = response.read()
-                print("Downloaded {} bytes from {}.".format(len(html), url))
-                data = lzma.decompress(html)
-                print("Extracted {} bytes with {} lines.".format(len(data), data.count(b"\n")))
-                if data.count(b"\n") > 10000:
-                        fout = open('full.json', 'wb')
-                        fout.write(data)
-                        fout.close()
-                        break
-                else:
-                        print("Seems too little data, retry.")
-        except (TypeError, IOError, ValueError, AttributeError):
-                        print("Failed, retry up to 10 times.")
+    try:
+        url = item.firstChild.nodeValue
+        response = urlopen(url)
+        html = response.read()
+        print("Downloaded {} bytes from {}.".format(len(html), url))
+        data = lzma.decompress(html)
+        print("Extracted {} bytes with {} lines.".format(len(data), data.count(b"\n")))
+        if data.count(b"\n") > 10000:
+            fout = open('full.json', 'wb')
+            fout.write(data)
+            fout.close()
+            break
+        else:
+            print("Seems too little data, retry.")
+    except (TypeError, IOError, ValueError, AttributeError):
+            print("Failed, retry up to 10 times.")
 
 #Convert and select
 fin = open('full.json', 'r')
@@ -69,51 +69,51 @@ lines = 0
 urls = {}
 url_duplicates = 0
 for line in fin:
-        lines+=1
-        try:
-                l = json.loads(line[8:-2])
-        except ValueError:
-                fail+=1
-                continue
-        if(l[0] != ''):
-                sender = str(l[0].encode("ascii","ignore").decode('ascii'))
-                sender2num[sender] = 1
-        else:
-                sender2num[sender] += 1
-        if(l[1] != ''):
-                thema = l[1]
-        titel = l[2]
-        datum = l[3]
-        zeit = l[4]
-        dauer = l[5]
-        beschreibung = l[7]
-        url = l[8]
-        website = l[9]
-        bild = l[10]
-        try:
-                datum_tm = time.strptime(datum, "%d.%m.%Y")
-                zeit_s = time.mktime(time.strptime(zeit, "%H:%M:%S")) - time.mktime(time.strptime("00:00:00", "%H:%M:%S"))
-                groesse_mb = float(l[6])
-        except ValueError:
-                fail+=1
-                continue
-        medium_from = time.localtime(time.time() - medium_minus_sec)
-        medium_to = time.localtime(time.time() + medium_plus_sec)
-        good_from = time.localtime(time.time() - good_minus_sec)
-        good_to = time.localtime(time.time() + good_plus_sec)
-        if(groesse_mb > min_filesize_mb and zeit_s > min_duration_sec):
-                if(url in urls):
-                        url_duplicates+=1
-                        continue
-                urls[url] = True
-                relevance = groesse_mb * 0.01
-                relevance += zeit_s * 0.0005
-                if(datum_tm > good_from and datum_tm < good_to):
-                        relevance += 100
-                elif(datum_tm > medium_from and datum_tm < medium_to):
-                        relevance += 40
-                dline = [sender, thema, titel, datum, zeit, dauer, beschreibung[:80], url, website, relevance]
-                output.append(dline)
+    lines+=1
+    try:
+        l = json.loads(line[8:-2])
+    except ValueError:
+        fail+=1
+        continue
+    if(l[0] != ''):
+        sender = str(l[0].encode("ascii","ignore").decode('ascii'))
+        sender2num[sender] = 1
+    else:
+        sender2num[sender] += 1
+    if(l[1] != ''):
+        thema = l[1]
+    titel = l[2]
+    datum = l[3]
+    zeit = l[4]
+    dauer = l[5]
+    beschreibung = l[7]
+    url = l[8]
+    website = l[9]
+    bild = l[10]
+    try:
+        datum_tm = time.strptime(datum, "%d.%m.%Y")
+        zeit_s = time.mktime(time.strptime(zeit, "%H:%M:%S")) - time.mktime(time.strptime("00:00:00", "%H:%M:%S"))
+        groesse_mb = float(l[6])
+    except ValueError:
+        fail+=1
+        continue
+    medium_from = time.localtime(time.time() - medium_minus_sec)
+    medium_to = time.localtime(time.time() + medium_plus_sec)
+    good_from = time.localtime(time.time() - good_minus_sec)
+    good_to = time.localtime(time.time() + good_plus_sec)
+    if(groesse_mb > min_filesize_mb and zeit_s > min_duration_sec):
+        if(url in urls):
+            url_duplicates+=1
+            continue
+        urls[url] = True
+        relevance = groesse_mb * 0.01
+        relevance += zeit_s * 0.0005
+        if(datum_tm > good_from and datum_tm < good_to):
+            relevance += 100
+        elif(datum_tm > medium_from and datum_tm < medium_to):
+            relevance += 40
+        dline = [sender, thema, titel, datum, zeit, dauer, beschreibung[:80], url, website, relevance]
+        output.append(dline)
 
 #Sort by relevance
 sorted_output = sorted(output, key=lambda tup: tup[-1], reverse=True)
