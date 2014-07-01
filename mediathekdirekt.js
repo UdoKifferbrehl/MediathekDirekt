0c09370b21e0c85d8d7c8a6588952bf149c70b31
Markus Koschany <apo@gambaru.de>
2014-07-01 21:32:11 +0200

    Add license paragraph to mediathekdirekt.js

--- mediathekdirekt.js
+++ mediathekdirekt.js
@@ -1,3 +1,21 @@
+/*
+Copyright 2014, martin776
+          2014, Markus Koschany
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var data = [];
 $.getJSON("good.json", function(jsondata)
 {
